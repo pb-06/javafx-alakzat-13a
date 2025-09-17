2683d7c6b6dfa14b3039a94e4ad9896d430a2ab2
pb-06 <kkandomail@gmail.com>
2025-09-17 13:00:11 +0200

    Show color

--- src/main/java/com/example/javafxalakzat13a/HelloController.java
+++ src/main/java/com/example/javafxalakzat13a/HelloController.java
@@ -1,14 +1,32 @@
 package com.example.javafxalakzat13a;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+    @FXML public ListView<String> listview_Listview;
+    @FXML public ImageView imageview_Alakzat;
+    @FXML public Pane pane_Alakzat;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void onPirosSelected(ActionEvent actionEvent) {
+        System.out.println("onPirosSelected actionEvent"+actionEvent);
+        pane_Alakzat.setBackground(new Background(new BackgroundFill(Color.RED, null, null)));
+    }
+
+    public void onZoldSelected(ActionEvent actionEvent) {
+        System.out.println("onZoldSelected actionEvent"+actionEvent);
+        pane_Alakzat.setBackground(new Background(new BackgroundFill(Color.GREEN, null, null)));
+    }
+
+    public void onBlueelected(ActionEvent actionEvent) {
+        System.out.println("onKekSelected actionEvent"+actionEvent);
+        pane_Alakzat.setBackground(new Background(new BackgroundFill(Color.BLUE, null, null)));
     }
 }
