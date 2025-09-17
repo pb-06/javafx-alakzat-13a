3eb94f964443554cc7186b6932f6d7e3fcd6146f
pb-06 <kkandomail@gmail.com>
2025-09-17 13:15:06 +0200

    Show shapes

--- src/main/java/com/example/javafxalakzat13a/HelloController.java
+++ src/main/java/com/example/javafxalakzat13a/HelloController.java
@@ -4,11 +4,15 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 public class HelloController {
     @FXML public ListView<String> listview_Listview;
@@ -16,17 +20,27 @@ public class HelloController {
     @FXML public Pane pane_Alakzat;
 
     public void onPirosSelected(ActionEvent actionEvent) {
-        System.out.println("onPirosSelected actionEvent"+actionEvent);
+        //System.out.println("onPirosSelected actionEvent"+actionEvent);
         pane_Alakzat.setBackground(new Background(new BackgroundFill(Color.RED, null, null)));
     }
 
     public void onZoldSelected(ActionEvent actionEvent) {
-        System.out.println("onZoldSelected actionEvent"+actionEvent);
         pane_Alakzat.setBackground(new Background(new BackgroundFill(Color.GREEN, null, null)));
     }
 
-    public void onBlueelected(ActionEvent actionEvent) {
-        System.out.println("onKekSelected actionEvent"+actionEvent);
+    public void onKekSelected(ActionEvent actionEvent) {
         pane_Alakzat.setBackground(new Background(new BackgroundFill(Color.BLUE, null, null)));
+    }
+
+    public void onNegyzetSelected(ActionEvent actionEvent) throws FileNotFoundException {
+        imageview_Alakzat.setImage(new Image("file:icons/negyzet.png"));
+    }
+
+    public void onKorSelected(ActionEvent actionEvent) throws FileNotFoundException {
+        imageview_Alakzat.setImage(new Image("file:icons/kor.png"));
+    }
+
+    public void onHaromszogSelected(ActionEvent actionEvent) throws FileNotFoundException {
+        imageview_Alakzat.setImage(new Image("file:icons/haromszog.png"));
     }
 }
